142abe11b94d118f367ddae76e6c7a22a8f80727
Jonathan Taylor <jon@stackhaus.com>
2014-05-27 18:01:28 +0100

    Initial hook for modifying damage done in correct place

--- lua/shine/extensions/equality.lua
+++ lua/shine/extensions/equality.lua
@@ -1,8 +1,11 @@
---The plugin table registered in shared.lua is passed in as the global "Plugin".
+Script.Load("lua/Class.lua")
+
 local Plugin = Plugin
 
 function Plugin:Initialise()
   self:CreateCommands()
+  self:OverrideMethods
+
 
   self.Enabled = true
 
@@ -18,6 +21,51 @@ end
 function Plugin:Cleanup()
   self.BaseClass.Cleanup( self )
 end
+
+
+function Plugin:OverrideMethods()
+  self.OverrideDoDamage(  )
+end
+
+
+
+
+
+
+-- Override NS2 Classes
+
+function Plugin:OverrideDoDamage()
+  local originalDoDamage
+  originalDoDamage = Class_ReplaceMethod( "DamageMixin", "DoDamage", 
+      function(damage, target, point, direction, surface, altMode, showtracer)
+          if Server then
+            // The doer is always self
+            local doer = self 
+            if self:isa("Player") then // Lets not nerf things like mines, whips, arcs
+              // Do damage reduction lookup here
+            end  
+          end
+            
+          originalDoDamage(damage, target, point, direction, surface, altMode, showtracer)
+      end
+  )
+end
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 Shine:RegisterExtension( "equality" , Plugin )
